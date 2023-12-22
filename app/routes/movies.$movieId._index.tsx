d9f4eff3dd6f167696b7e4b570a4663b4f491541
paulweezydesign <86060999+paulweezydesign@users.noreply.github.com>
2023-12-22 06:09:35 -0700

    Initial commit

---
+++ app/routes/movies.$movieId._index.tsx
@@ -0,0 +1,90 @@
+import { Link, useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
+
+const axios = require("axios");
+
+export let loader = async ({ params }) => {
+  invariant(params.title, "expected params.title");
+
+  let data = JSON.stringify({
+    collection: "movies",
+    database: "sample_mflix",
+    dataSource: process.env.CLUSTER_NAME,
+    filter: { title: params.title }
+  });
+
+  let config = {
+    method: "post",
+    url: process.env.DATA_API_BASE_URL + "/action/findOne",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Request-Headers": "*",
+      "api-key": process.env.DATA_API_KEY
+    },
+    data
+  };
+
+  let result = await axios(config);
+  let movie = result?.data?.document || {};
+
+  let poster = movie?.poster ||
+    "https://image.shutterstock.com/z/stock-vector-black-linear-photo-camera-logo-like-no-image-available-flat-stroke-style-trend-modern-logotype-art-622639151.jpg";
+
+  return {
+    title: params.title,
+    plot: movie.fullplot,
+    genres: movie.genres,
+    directors: movie.directors,
+    year: movie.year,
+    image: poster
+  };
+};
+
+export default function MovieDetails() {
+  let movie = useLoaderData();
+
+  return (
+    <div>
+      <h1>{movie.title}</h1>
+      {movie.plot}
+      <br></br>
+      <div styles="padding: 25% 0;" class="tooltip">
+        <li>Year</li>
+        <Link
+          class="tooltiptext"
+          to={"../movies?filter=" + JSON.stringify({ year: movie.year })}
+        >
+          {movie.year}
+        </Link>
+      </div>
+      <br />
+      <div styles="padding: 25% 0;" class="tooltip">
+        <li>Genres</li>
+        <Link
+          class="tooltiptext"
+          to={"../movies?filter=" + JSON.stringify({ genres: movie.genres })}
+        >
+          {movie.genres.map((genre) => {
+            return genre + " | ";
+          })}
+        </Link>
+      </div>
+      <br />
+      <div styles="padding: 25% 0;" class="tooltip">
+        <li>Directors</li>
+        <Link
+          class="tooltiptext"
+          to={
+            "../movies?filter=" + JSON.stringify({ directors: movie.directors })
+          }
+        >
+          {movie.directors.map((director) => {
+            return director + " | ";
+          })}
+        </Link>
+      </div>
+      <br></br>
+      <img src={movie.image}></img>
+    </div>
+  );
+}
---
+++ app/routes/movies.tsx
@@ -0,0 +1,130 @@
+import { Form, Link, useLoaderData, useSearchParams, useSubmit } from "@remix-run/react";
+
+const axios = require("axios");
+
+export let loader = async ({ request }) => {
+  let url = new URL(request.url);
+  let searchTerm = url.searchParams.get("searchTerm");
+  let filter = JSON.parse(url.searchParams.get("filter"));
+
+  let pipeline = [];
+  if (searchTerm) {
+    pipeline = [
+      {
+        $search: {
+          text: {
+            query: searchTerm,
+            path: {
+              wildcard: "*"
+            }
+          }
+        }
+      },
+      { $limit: 100 },
+      { $addFields: { meta: "$$SEARCH_META" } }
+    ];
+  } else if (filter) {
+    pipeline = [
+      {
+        $match: filter
+      },
+      { $limit: 100 }
+    ];
+  } else {
+    pipeline = [{ $limit: 100 }];
+  }
+
+  let data = JSON.stringify({
+    collection: "movies",
+    database: "sample_mflix",
+    dataSource: process.env.CLUSTER_NAME,
+    pipeline
+  });
+
+  let config = {
+    method: "post",
+    url: process.env.DATA_API_BASE_URL + "/action/aggregate",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Request-Headers": "*",
+      "api-key": process.env.DATA_API_KEY
+    },
+    data
+  };
+
+  let movies = await axios(config);
+  let totalFound = filter ?  await getCountMovies(filter) : await getCountMovies();
+
+  return {
+    showCount: movies?.data?.documents?.length,
+    totalCount: totalFound,
+    documents: movies?.data?.documents
+  };
+};
+
+const getCountMovies = async (countFilter) => {
+  let pipeline = countFilter ?
+    [{ $match: countFilter }, { $count: "count" }] :
+    [{ $count: "count" }];
+
+  let data = JSON.stringify({
+    collection: "movies",
+    database: "sample_mflix",
+    dataSource: process.env.CLUSTER_NAME,
+    pipeline: pipeline
+  });
+
+  let config = {
+    method: "post",
+    url: process.env.DATA_API_BASE_URL + "/action/aggregate",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Request-Headers": "*",
+      "api-key": process.env.DATA_API_KEY
+    },
+    data: data
+  };
+
+  let result = await axios(config);
+
+  return result?.data?.documents[0]?.count;
+};
+
+export default function Movies() {
+  let [searchParams, setSearchParams] = useSearchParams();
+
+  let submit = useSubmit();
+  let movies = useLoaderData();
+  let totalFound = movies.totalCount;
+  let totalShow = movies.showCount;
+  if (movies.documents[0]?.meta?.count?.lowerBound) {
+    totalFound = movies.documents[0]?.meta?.count?.lowerBound;
+    if (totalFound < totalShow) {
+      totalShow = totalFound;
+    }
+  }
+
+  return (
+    <div>
+      <h1>Movies</h1>
+      <Form method="get">
+        <input
+          onChange={(e) => submit(e.currentTarget.form)}
+          id="searchBar"
+          name="searchTerm"
+          placeholder="Search movies..."
+        />
+        <p>
+          Showing {totalShow} of total {totalFound} movies found
+        </p>
+      </Form>
+      <ul>
+        {movies.documents.map((movie) => (
+          <li key={movie.title}>
+            <Link to={movie.title}>{movie.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
